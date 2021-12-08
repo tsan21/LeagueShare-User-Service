09556031b1365984a87f96df665794935ec8b238
tsan <tsanyip@hotmail.com>
2021-12-08 19:55:30 +0100

    rabbit

--- src/main/java/leagueshare/userservice/controllers/UserController.java
+++ src/main/java/leagueshare/userservice/controllers/UserController.java
@@ -2,31 +2,38 @@ package leagueshare.userservice.controllers;
 
 import leagueshare.userservice.entities.User;
 import leagueshare.userservice.repos.UserRepo;
+import leagueshare.userservice.rmq.MessagingConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/user")
 public class UserController {
 
-    private UserRepo userRepo;
+    private final UserRepo userRepo;
+    private final RabbitTemplate template;
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    public UserController(UserRepo userRepo) {
+    public UserController(UserRepo userRepo, RabbitTemplate template) {
         this.userRepo = userRepo;
+        this.template = template;
     }
 
 
-    @GetMapping("/")
+    @PostMapping("/")
     public ResponseEntity<?> create() {
         User user = new User("user","user@user.com","user","resu");
         userRepo.save(user);
+        template.convertAndSend(MessagingConfig.topicExchangeName, MessagingConfig.routingKey, user);
+        log.info("Created new user: " + user);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
---
+++ src/main/java/leagueshare/userservice/rmq/MessagingConfig.java
@@ -0,0 +1,45 @@
+package leagueshare.userservice.rmq;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+
+@Configuration
+public class MessagingConfig {
+
+    public static final String topicExchangeName = "leagueshare-exchanger";
+    public static final String queueName = "user-queue";
+    public static final String routingKey = "leagueshare-key";
+
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
